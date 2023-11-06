e53d4136af0ed400263035f17f50d52bbd7dfcce
ghadah2019 <ghada__o@hotmail.com>
2023-11-06 16:24:49 +0300

    qq

---
+++ main.js
@@ -0,0 +1,101 @@
+
+let img1 = document.getElementById("image1")
+let b1 = document.getElementById("one")
+let b2 = document.getElementById("two")
+let b3 = document.getElementById("three")
+let b4 = document.getElementById("four")
+let b5 = document.getElementById("five")
+let b6 = document.getElementById("six")
+let b7 = document.getElementById("seven")
+let b8 = document.getElementById("eight")
+let b9 = document.getElementById("nine")
+
+function channel1 (){
+    b1.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+
+
+    })
+} channel1();
+
+function channel2 (){
+    b2.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+
+    })
+} channel2();
+
+
+function channel3 (){
+    b3.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+
+    })
+} channel3();
+
+
+
+function channel4 (){
+    b4.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+    
+
+    })
+} channel4();
+
+
+
+function channel5 (){
+    b5.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+        
+    
+
+    })
+} channel5();
+
+
+
+function channel6 (){
+    b6.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+        
+    
+
+    })
+} channel6();
+
+
+
+function channel7 (){
+    b7.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+        
+    
+
+    })
+} channel7();
+
+
+function channel8 (){
+    b8.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+    
+
+    })
+} channel8();
+
+
+
+function channel9 (){
+    b9.addEventListener('click', function myfunc(){
+        img1.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+
+    })
+} channel9();
+
+
+
+
+
+
